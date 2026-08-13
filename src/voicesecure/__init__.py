812c60c4b77a51a94f3dbaa3b8925a2008c9a57e
dleha <di913505@gmail.com>
2026-08-13 13:50:19 +0900

    chore: remove legacy SDK code from integration branch

--- src/voicesecure/__init__.py
+++
@@ -1,3 +0,0 @@
-"""VoiceSecure SDK - Voice anonymization with RL-learned modulation."""
-
-__version__ = "0.1.0"
--- tests/test_placeholder.py
+++
@@ -1,8 +0,0 @@
-"""Placeholder test to ensure CI works."""
-
-
-def test_import():
-    """Verify package can be imported."""
-    import voicesecure
-
-    assert voicesecure.__version__ == "0.1.0"
